d0f37bdf1396f6cfd4675315e9a4963a9cb76b41
nawafpc <159987882+nawafpc@users.noreply.github.com>
2024-10-07 15:42:24 +0300

    Update MyViewView.java

--- src/main/java/e2e/community/views/myview/MyViewView.java
+++ src/main/java/e2e/community/views/myview/MyViewView.java
@@ -27,6 +27,7 @@ import java.util.List;
 @PageTitle("My View")
 @Menu(icon = "line-awesome/svg/pencil-ruler-solid.svg", order = 0)
 @Route("")
+    @StyleSheet("frontend/themes/e2e-company/styles.css")
 public class MyViewView extends Composite<VerticalLayout> {
 
     public MyViewView() {
